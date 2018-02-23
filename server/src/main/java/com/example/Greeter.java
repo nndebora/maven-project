82d60fb04367483fad8ffe38cdda4970715ef933
Debora Nascimento <dnon@documentsolutions.com.br>
2018-02-23 14:38:56 -0300

    Ajuste de chekstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -3,20 +3,24 @@ package com.example;
 /**
  * This is a class.
  */
-public class Greeter {
+public class Greeter
+{
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
+    /**
+     * This is a constructor.
+     */
+    public Greeter()
+    {
 
-  }
+    }
 
-  /**
-   * @param someone the name of a person
-   * @return greeting string
-   */
-  public final String greet(final String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+    /**
+     * @param someone
+     *            the name of a person
+     * @return greeting string
+     */
+    public final String greet(final String someone)
+    {
+        return String.format("Hello, %s!", someone);
+    }
 }
